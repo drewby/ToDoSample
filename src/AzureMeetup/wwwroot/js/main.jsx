198df971899b4df9c0407a2b7054c33a38896c2c
drew <drew@ROBBINS-PC>
2016-09-25 12:09:40 +0900

    Added react code, not wired yet

---
+++ src/AzureMeetup/wwwroot/js/main.jsx
@@ -0,0 +1,57 @@
+﻿var data = [
+  { id: 1, description: "This is one todo" },
+  { id: 2, description: "This is *another* todo" }
+];
+
+var ToDoItem = React.createClass({
+    render: function () {
+        return(
+            <div className="todo">
+                {this.props.id} 
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+var ToDoList = React.createClass({
+    render: function () {
+        var todoNodes = this.props.data.map(function (todo) {
+            return (
+                <ToDoItem id={todo.id}>{todo.description}</ToDoItem>    
+            );
+        });
+        return (
+          <div className="todoList">
+            {todoNodes}
+          </div>
+      );
+    }
+});
+
+var ToDoForm = React.createClass({
+    render: function () {
+        return (
+            <div className="todoForm">
+                This is the ToDo Form.
+            </div>
+        );
+    }
+})
+
+var ToDo = React.createClass({
+    render: function () {
+        return (
+            <div className="todo">
+                <h3>To Do</h3>
+                <ToDoList data={this.props.data}/>
+                <ToDoForm />
+            </div>
+        );
+    }
+})
+
+ReactDOM.render(
+  <ToDo data={data} />,
+  document.getElementById('content')
+);
